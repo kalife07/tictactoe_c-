c29effeabd2846c712e5994ee4134e059432f5e7
Adel Khalife <adel@Adels-MacBook-Air.local>
2023-12-03 17:50:47 +0200

    les tours des joueurs

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-int boardint[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+int boardint[3][3];
 
 void jouer(char joueur, char board[3][3], string jn, int boardint[3][3]) {
     int posint;
     cout <<"("<<jn<<")"<<" Entrer la position: "<<endl;
     cin >> posint;
-    while (posint<=1 || posint>=9) {
+    while (posint<1 || posint>9) {
         cout << "invalid input"<<endl;
         cout << "Entrer la position: "<<endl;
         cin >> posint;
@@ -17,28 +17,31 @@ void jouer(char joueur, char board[3][3], string jn, int boardint[3][3]) {
 
     for (int i=0;i<3;i++) {
         for (int j=0;j<3;j++) {
-            if (posint==boardint[i][j]) {
-                board[i][j] = joueur;
+            if(1<=posint && posint<=3) {
+                if(board[0][j]=='0'+posint){
+                    board[0][j] = joueur;
+                }
+            }
+            else if(4<=posint && posint<=6) {
+
+                if(board[1][j]=='0'+posint){
+                    board[1][j] = joueur;
+                    break;
+                }
+            }
+            else {
+                if(board[2][j]=='0'+posint){
+                    board[2][j] = joueur;
+                }
             }
         }
     }
+        
     
     
 }
 
 void board_ui(){
-    /*for (int i=0;i<sizeof(board);i++) {
-        for (int j=0;j<sizeof(board[i]);j++) {
-            cout<<board[j];
-            if (j%3!=0 && j!=0) {
-                cout<<"|";
-            }
-            if (j%3==0) {
-                cout<<endl;
-            }
-        }
-    }*/
-    //cout<<board;
     cout<<"\t  "<<board[0][0]<<"  |  "<<board[0][1]<<"  |  "<<board[0][2]<<"\n";
     cout<<"\t-----------------"<<endl;
     cout<<"\t  "<<board[1][0]<<"  |  "<<board[1][1]<<"  |  "<<board[1][2]<<"\n";
@@ -53,6 +56,10 @@ void board_ui(){
     bool gameover = false;
     int round = 1;
     while (!gameover) {
+        if (round>9) {
+            cout << "Fin du jeu"<<endl;
+            break;
+        }
         cout << endl;
         if (round%2!=0) {
             jouer('X',board,j1,boardint);
