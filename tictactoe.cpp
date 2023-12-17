02ec1279576e97489277bb528515cdcea964c29f
Adel Khalife <adel@Adels-MacBook-Air.local>
2023-12-17 17:49:05 +0200

    final

--- tictactoe.cpp
+++ tictactoe.cpp
@@ -5,6 +5,46 @@ using namespace std;
 char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
 int boardint[3][3];
 
+bool fin(char board[3][3], char joueur) {
+    int index = 0;
+    for (int i=0;i<3;i++) {
+        for (int j=0;j<3;j++) {
+            //cout<<"index: "<<index<<endl;
+            if (board[i][j]==joueur) {
+                index++;
+            }        
+        }
+        if (index==3) {
+            return true;
+        }
+        index = 0;
+    }
+
+    for (int i=0;i<3;i++) {
+        for (int j=0;j<3;j++) {
+            if (board[j][i]==joueur) {
+                index++;
+            }        
+        }
+        if (index==3) {
+            return true;
+        }
+        index = 0;
+    }
+
+    if (board[0][0]==board[1][1] && board[1][1]==board[2][2]) {
+        //cout<<"test gagnant"<<endl;
+        return true;
+    }
+
+    if (board[0][2]==board[1][1] && board[1][1]==board[2][0]) {
+        //cout<<"test gagnant"<<endl;
+        return true;
+    }
+
+    return false;
+}
+
 void jouer(char joueur, char board[3][3], string jn, int boardint[3][3]) {
     int posint;
     cout <<"("<<jn<<")"<<" Entrer la position: "<<endl;
@@ -63,9 +103,17 @@ void board_ui(){
         cout << endl;
         if (round%2!=0) {
             jouer('X',board,j1,boardint);
+            if (fin(board,'X')) {
+                cout<<j1<<" a gagne!"<<endl;
+                gameover = true;
+            }
         }
         else {
             jouer('O',board,j2,boardint);
+            if (fin(board,'O')) {
+                cout<<j2<<" a gagne!"<<endl;
+                gameover = true;
+            }
         }
         cout<<"\t  "<<board[0][0]<<"  |  "<<board[0][1]<<"  |  "<<board[0][2]<<"\n";
         cout<<"\t-----------------"<<endl;
